8517e02fcbb61062a4a24842dae85ca11963bb0d
SomayPawa <lalpawasomay@gmail.com>
2025-02-26 09:54:04 +0530

    first commit

---
+++ Src/Variable.java
@@ -0,0 +1,13 @@
+public class Variable{
+    public static void main(String [] args){
+        String name = "somay";
+        int number = 50;
+        int value = 10;
+        System.out.println(name);
+        System.out.println(value);
+        System.out.println(number);
+        boolean haa = true;
+        System.out.println(haa);
+    }
+}
+
